c853a31e1f69b4266967ff43ec549e1f36494fb4
rezaramadhanirianto <rezaramadhanirianto@gmail.com>
2021-09-29 22:13:04 +0700

    First Commit

---
+++ src/main/java/com/froyout/LearnrestfulapiApplication.java
@@ -0,0 +1,13 @@
+package com.froyout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnrestfulapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnrestfulapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/froyout/learnrestfulapi/LearnrestfulapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.froyout.learnrestfulapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnrestfulapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

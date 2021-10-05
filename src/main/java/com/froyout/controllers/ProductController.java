f0e18a5110f2108373e63abb8c6f31a3127cafc1
rezaramadhanirianto <rezaramadhanirianto@gmail.com>
2021-10-05 20:47:53 +0700

    add validation

--- src/main/java/com/froyout/controllers/ProductController.java
+++ src/main/java/com/froyout/controllers/ProductController.java
@@ -1,11 +1,17 @@
 package com.froyout.controllers;
 
+import com.froyout.dto.ResponseData;
 import com.froyout.models.entities.Product;
 import com.froyout.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -16,8 +22,19 @@ public class ProductController {
     private ProductService productService;
 
     @PostMapping(consumes = MediaType.ALL_VALUE)
-    public Product create(Product product){
-        return productService.save(product);
+    public ResponseEntity<ResponseData<Product>> create(@Valid Product product, Errors errors){
+        ResponseData<Product> responseData = new ResponseData<>();
+        if(errors.hasErrors()){
+            for(ObjectError error: errors.getAllErrors()){
+                responseData.getMessage().add(error.getDefaultMessage());
+            }
+            responseData.setStatus(false);
+            responseData.setPayLoad(null);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+        }
+        responseData.setStatus(true);
+        responseData.setPayLoad(productService.save(product));
+        return ResponseEntity.ok(responseData);
     }
 
     @GetMapping
@@ -26,13 +43,24 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
-    public Product findOne( @PathVariable("id") Long id){
+    public Product findOne(@PathVariable("id") Long id){
         return productService.findOne(id);
     }
 
     @PutMapping(consumes = MediaType.ALL_VALUE)
-    public Product update(Product product){
-        return productService.save(product);
+    public ResponseEntity<ResponseData<Product>> update(@Valid Product product, Errors errors){
+        ResponseData<Product> responseData = new ResponseData<>();
+        if(errors.hasErrors()){
+            for(ObjectError error: errors.getAllErrors()){
+                responseData.getMessage().add(error.getDefaultMessage());
+            }
+            responseData.setStatus(false);
+            responseData.setPayLoad(null);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+        }
+        responseData.setStatus(true);
+        responseData.setPayLoad(productService.save(product));
+        return ResponseEntity.ok(responseData);
     }
 
     @DeleteMapping("/{id}")
---
+++ src/main/java/com/froyout/dto/ResponseData.java
@@ -0,0 +1,34 @@
+package com.froyout.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResponseData<T> {
+    private boolean status;
+    private List<String> message = new ArrayList<String>();
+    private T payLoad;
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public List<String> getMessage() {
+        return message;
+    }
+
+    public void setMessage(List<String> message) {
+        this.message = message;
+    }
+
+    public T getPayLoad() {
+        return payLoad;
+    }
+
+    public void setPayLoad(T payLoad) {
+        this.payLoad = payLoad;
+    }
+}
